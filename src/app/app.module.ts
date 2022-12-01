84e9086da7d7d4b7a8fb49f1f77e604a1aa5b989
Hugo Huzé--Cardosi <hugo.cardosi@hotmail.fr>
2022-12-01 09:50:29 +0100

    generate component home, login and register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomComponent } from './hom/hom.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
